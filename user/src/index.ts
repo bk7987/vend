f8ed1bcd0e16c570f868a3113fdac732ccc73192
Bill Kerr <48515765+bk7987@users.noreply.github.com>
2020-08-10 16:01:57 -0400

    User service up and running

---
+++ user/src/index.ts
@@ -0,0 +1,40 @@
+import * as mongoose from "mongoose";
+import * as firebase from "firebase-admin";
+import { app } from "./app";
+
+const startApp = async () => {
+  checkEnvironment("MONGO_URI", "PORT", "FIREBASE_CONFIG");
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("User service connected to MongoDB.");
+  } catch (error) {
+    console.error(error);
+  }
+
+  try {
+    firebase.initializeApp();
+    console.log("User service Firebase authentication initialized.");
+  } catch (error) {
+    console.error(error);
+  }
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`User service listening on port ${port}.`);
+  });
+};
+
+const checkEnvironment = (...args: string[]) => {
+  args.forEach((arg) => {
+    if (!process.env[arg]) {
+      throw new Error(`${arg} is not defined.`);
+    }
+  });
+};
+
+startApp();
